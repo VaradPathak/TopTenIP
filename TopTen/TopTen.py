560c78e678cfeb8f224972ac5f0d1179578024b3
Varad Pathak <pathak.varad@gmail.com>
2014-05-11 21:24:09 -0700

    Changes made to make generalized scripts

--- TopTen/TopTen.py
+++ TopTen/TopTen.py
@@ -4,6 +4,7 @@ import dateutil.tz
 import sys
 
 
+# Convert Apache time format to datatime object
 def my_apachetime(s):
     month_map = {'Jan': 1, 'Feb': 2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7,
         'Aug':8, 'Sep': 9, 'Oct':10, 'Nov': 11, 'Dec': 12}
---
+++ TopTen/main.py
@@ -0,0 +1,62 @@
+import os
+import pysftp
+import sys
+
+
+if __name__ == "__main__":
+    serverid = 0
+    IPAddressDict = {}
+    hitsDict = {}
+#     Collect top ten results from each server
+    for line in open('ConfigServers', 'r'):
+        line = line.strip() 
+        h, usr, pwd, logFilePath = line.split()
+        serverid += 1
+        print 'Connecting ' + ' ' + h
+        try:
+            srv = pysftp.Connection(host=h, username=usr, password=pwd)
+        except:
+            print 'Could Not connect to '+h
+            continue
+        srv.put('TopTen.py')
+        command = 'python TopTen.py ' + logFilePath
+        # Execute the script on the server
+        srv.execute(command)
+        # Download the file from the remote server
+        print sys.argv[1] + '/result_' + str(serverid)
+        print srv.listdir()
+        srv.get('TopTenIPResult', sys.argv[1] + '/result_' + str(serverid))
+        srv.execute('rm TopTen.py')
+        srv.execute('rm TopTenIPResult')
+        # Closes the connection
+        srv.close()
+    
+#     Create a map of results collected from all the servers
+    for resultId in range(1, serverid + 1):
+        filename = sys.argv[1] + '/result' + str(resultId)
+        for line in open(filename, 'r'):
+            line = line.strip() 
+            ip, counts, hits = line.split()
+            if ip in IPAddressDict:
+                IPAddressDict[ip] += counts
+                hitsDict[ip] += hits
+            else:
+                IPAddressDict[ip] = counts
+                hitsDict[ip] = hits
+        
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+    
+    # Output the final result to console
+    count = 0
+    print ('ipAddress' + '\t' + 'count' + '\t' + 'hits' + '\t' + 'hitRate')
+    for ip in sorted(IPAddressDict, key=IPAddressDict.get, reverse=True):
+        if ip in hitsDict:
+            print (ip + '\t' + str(IPAddressDict[ip]) + '\t' + str(hitsDict[ip])) + '\t' + str(float(hitsDict[ip]) / float(IPAddressDict[ip]))
+        else:
+            print ip + '\t' + str(IPAddressDict[ip]) + '\t' + str(0) + '\t' + str(0)
+        count += 1
+        if(count == 10):
+            break
